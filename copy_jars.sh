c74f0276b6ed7040bf9cf231e7b130a760afc707
Mark Bittmann <mark.bittmann@lookout.com>
2016-02-29 14:54:29 -0500

    ability to get all external libs for running as a jar file

---
+++ copy_jars.sh
@@ -0,0 +1,6 @@
+cp lib_managed/jars/*/*/*.jar libs/
+cp mypipe-api/target/scala-2.11/api_2.11-1.0.0.jar libs
+cp mypipe-avro/target/scala-2.11/myavro_2.11-1.0.0.jar libs/
+cp mypipe-kafka/target/scala-2.11/mykafka_2.11-1.0.0.jar libs/
+cp mypipe-producers/target/scala-2.11/producers_2.11-1.0.0.jar libs/
+
---
+++ run_replicator.sh
@@ -0,0 +1,1 @@
+java -cp mypipe-runner/target/scala-2.11/runner_2.11-1.0.0.jar:libs/* -Dconfig.file=conf/local_application.conf mypipe.runner.PipeRunner
